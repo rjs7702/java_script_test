b2784fe0c8eef991d451422b534d2d8c517bb8b0
rjs7702 <arav.rjs77@gmail.com>
2026-08-19 17:20:34 +0530

    Update 03_operationOperators.js

--- .devcontainer/01_basics/03_operationOperators.js
+++ .devcontainer/01_basics/03_operationOperators.js
@@ -1,39 +1,68 @@
-let score=33;
-let score2="33";
-let score3="33abc";
-let score4=true;
+// let score=33;
+// let score2="33";
+// let score3="33abc";
+// let score4=true;
 
-let result=Number(score) //33
-let result2=Number(score2) //33
-let result3=Number(score3) //NaN
-let result4=Number(score4) //1
+// let result=Number(score) //33
+// let result2=Number(score2) //33
+// let result3=Number(score3) //NaN
+// let result4=Number(score4) //1
 
-console.log(result)
-console.log(result2)
-console.log(result3)
-console.log(result4)
+// console.log(result)
+// console.log(result2)
+// console.log(result3)
+// console.log(result4)
 
-let answer=String(score) //33
-let answer2=String(score2) //"33"
-let answer3=String(score3) //"33abc"
-let answer4=String(score4) //"true"
+// let answer=String(score) //33
+// let answer2=String(score2) //"33"
+// let answer3=String(score3) //"33abc"
+// let answer4=String(score4) //"true"
 
-console.log(answer)
-console.log(answer2)
-console.log(answer3)
-console.log(answer4)
+// console.log(answer)
+// console.log(answer2)
+// console.log(answer3)
+// console.log(answer4)
 
-let finalResult=Boolean(score) //true
-let finalResult2=Boolean(score2) //true
-let finalResult3=Boolean(score3) //true
-let finalResult4=Boolean(score4) //true 
-console.log(finalResult)
-console.log(finalResult2)
-console.log(finalResult3)
-console.log(finalResult4)   
+// let finalResult=Boolean(score) //true
+// let finalResult2=Boolean(score2) //true
+// let finalResult3=Boolean(score3) //true
+// let finalResult4=Boolean(score4) //true 
+// console.log(finalResult)
+// console.log(finalResult2)
+// console.log(finalResult3)
+// console.log(finalResult4)   
 
 
-let someNumber=33;
-let stringNumber=String(someNumber) 
-console.log(typeof someNumber) //number
-console.log(typeof stringNumber) //string
+// let someNumber=33;
+// let stringNumber=String(someNumber) 
+// console.log(typeof someNumber) //number
+// console.log(typeof stringNumber) //string
+
+
+
+//************Operations*****************/
+
+let value=3;
+let negValue= -value;
+// console.log(negValue); 
+// console.log(2+2)
+// console.log(2-1)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3)
+
+let str1="Hello"
+let str2="World"
+// console.log(str1+str2) //HelloWorld
+// console.log(str1+" "+str2) //Hello World
+
+console.log("1" + 1) //11
+console.log(1 + "1") //11
+console.log("1" + 1 + 1) //111
+console.log(1 + 1 + "1") //21
+console.log("1" + 1 + 1) //111
+
+
+
+
