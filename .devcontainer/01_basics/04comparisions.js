a0a21a5a4d7cf8db675b51a7907a560c16c84ce1
rjs7702 <arav.rjs77@gmail.com>
2026-08-19 17:42:58 +0530

    comparisions

---
+++ .devcontainer/01_basics/04comparisions.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2>=1)
+// console.log(2<=1)
+
+
+// console.log("2">1); 
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);//equality operator works differently than relational operators. It will return false because null is only equal to undefined and not equal to any other value.
+// console.log(null>=0);//relational operator converts null to 0 before comparison.
+
+// console.log(undefined>0);
+// console.log(undefined==0);
+// console.log(undefined>=0);
+
+//====>strict equality operator
+console.log(2==="2") //false
+console.log(2==="2") //false
+console.log(2!=="2") //true because 2 is number and "2" is string
