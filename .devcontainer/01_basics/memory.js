529f370672856120bc93ac24db7f9793e95d49b5
rjs7702 <arav.rjs77@gmail.com>
2026-08-20 17:21:52 +0530

    memoryconcept in js

---
+++ .devcontainer/01_basics/memory.js
@@ -0,0 +1,11 @@
+//stack(Primitive datatypes) , heap(Non- Primitive)
+
+let myName="Raju Singh"
+
+let anotherName= myName
+
+anotherName="Chhotu"
+
+console.log(myName);
+
+console.log(anotherName);
